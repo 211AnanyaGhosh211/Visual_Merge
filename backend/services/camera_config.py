59c6798feec9dd092d1052d24948a9098bf56a40
Avijit Patra <avijit.eframe@gmail.com>
2026-01-12 11:38:50 +0530

    camera ip updated

--- backend/services/camera_config.py
+++ backend/services/camera_config.py
@@ -4,68 +4,68 @@
 
 CAMERA_CONFIG = {
     "0": {
-        "name": "Laptop Camera", 
-        "type": "laptop", 
+        "name": "Laptop Camera",
+        "type": "laptop",
         "url": None,
         "description": "Built-in laptop webcam"
     },
     "1": {
-        "name": "Main Entrance", 
-        "type": "rtsp", 
-        "url": "rtsp://admin:admin@1966@192.168.100.109:554/cam/realmonitor?channel=4&subtype=0",
+        "name": "Main Entrance",
+        "type": "rtsp",
+        "url": "rtsp://admin:admin@1966@192.168.100.103:554/cam/realmonitor?channel=4&subtype=0",
         "description": "Main entrance camera"
     },
     "2": {
-        "name": "Parking Lot", 
-        "type": "rtsp", 
+        "name": "Parking Lot",
+        "type": "rtsp",
         "url": "rtsp://admin:admin%401966@192.168.100.112:554/cam/realmonitor?channel=2&subtype=0",
         "description": "Parking lot camera"
     },
     "3": {
-        "name": "Workshop Area", 
-        "type": "rtsp", 
+        "name": "Workshop Area",
+        "type": "rtsp",
         "url": "rtsp://admin:admin%401966@192.168.100.112:554/cam/realmonitor?channel=3&subtype=0",
         "description": "Workshop area camera"
     },
     "4": {
-        "name": "Office Area", 
-        "type": "rtsp", 
+        "name": "Office Area",
+        "type": "rtsp",
         "url": "rtsp://admin:admin%401966@192.168.100.112:554/cam/realmonitor?channel=4&subtype=0",
         "description": "Office area camera"
     },
     "5": {
-        "name": "Storage Area", 
-        "type": "rtsp", 
+        "name": "Storage Area",
+        "type": "rtsp",
         "url": "rtsp://admin:admin%401966@192.168.100.112:554/cam/realmonitor?channel=5&subtype=0",
         "description": "Storage area camera"
     },
     "6": {
-        "name": "Loading Dock", 
-        "type": "rtsp", 
+        "name": "Loading Dock",
+        "type": "rtsp",
         "url": "rtsp://admin:admin%401966@192.168.100.112:554/cam/realmonitor?channel=6&subtype=0",
         "description": "Loading dock camera"
     },
     "7": {
-        "name": "Break Room", 
-        "type": "rtsp", 
+        "name": "Break Room",
+        "type": "rtsp",
         "url": "rtsp://admin:admin%401966@192.168.100.112:554/cam/realmonitor?channel=7&subtype=0",
         "description": "Break room camera"
     },
     "8": {
-        "name": "Conference Room", 
-        "type": "rtsp", 
+        "name": "Conference Room",
+        "type": "rtsp",
         "url": "rtsp://admin:admin%401966@192.168.100.112:554/cam/realmonitor?channel=8&subtype=0",
         "description": "Conference room camera"
     },
     "9": {
-        "name": "Reception Area", 
-        "type": "rtsp", 
+        "name": "Reception Area",
+        "type": "rtsp",
         "url": "rtsp://admin:admin%401966@192.168.100.112:554/cam/realmonitor?channel=9&subtype=0",
         "description": "Reception area camera"
     },
     "10": {
-        "name": "Emergency Exit", 
-        "type": "rtsp", 
+        "name": "Emergency Exit",
+        "type": "rtsp",
         "url": "rtsp://admin:admin%401966@192.168.100.112:554/cam/realmonitor?channel=10&subtype=0",
         "description": "Emergency exit camera"
     }
@@ -78,8 +78,8 @@ CAMERA_CONFIG = {
 
 # Example:
 # "11": {
-#     "name": "New Camera", 
-#     "type": "rtsp", 
+#     "name": "New Camera",
+#     "type": "rtsp",
 #     "url": "rtsp://username:password@192.168.1.100:554/stream1",
 #     "description": "Your new camera description"
 # }
